029463b2a92b083aec7de38a225f44ade21a234c
Piyush <piyush.dongre16@gmail.com>
2020-09-29 19:31:35 +0530

    Updates

---
+++ precache-manifest.c564b949dc85cfc2b6b790666fd05970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b6f3e3a1a82dbcea81be84039061730",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "a40c4828351eb7e47d6d",
+    "url": "/expense-tracker/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "d274357ae37698f69cd8",
+    "url": "/expense-tracker/static/js/2.87902a96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.87902a96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a40c4828351eb7e47d6d",
+    "url": "/expense-tracker/static/js/main.408706e9.chunk.js"
+  },
+  {
+    "revision": "2a39391c79534067f41a",
+    "url": "/expense-tracker/static/js/runtime-main.84524912.js"
+  }
+]);
